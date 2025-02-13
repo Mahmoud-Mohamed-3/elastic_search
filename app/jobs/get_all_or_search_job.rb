f90fe76ec1deb5590fe80b5506d4185292898ea5
Mahmoud-Mohamed-3 <mahmoudfalous@gmail.com>
2025-02-14 01:31:52 +0200

    Make It Faster

--- app/jobs/get_all_or_search_job.rb
+++ app/jobs/get_all_or_search_job.rb
@@ -7,30 +7,39 @@ class GetAllOrSearchJob
   def perform(query = nil)
     Sidekiq.logger.info ">>>>>> Job started with query: #{query.inspect}"
 
+
     purchase_orders = if query.present?
-                        PurchaseOrder.search(
-                          query: {
-                            bool: {
-                              should: [
-                                { multi_match: {
-                                  query: query,
-                                  fields: [
-                                    "order_num", "status", "sales_channel",
-                                    "payment_method"
-
-                                  ],
-                                  type: "best_fields",
-                                  fuzziness: "AUTO"
-                                }
-                                }
-                              ],
-                              minimum_should_match: 1
-                            }
-                          },
-                          size: 10000
-                          # size: 10000
-
-                        ).records.to_a
+                        if [ "unshipped", "shipped", "confirmed" ].include?(query.downcase)
+                          PurchaseOrder.search(
+                            query: {
+                              bool: {
+                                filter: [
+                                  { term: { "status.lower" => query.downcase } }
+                                ]
+                              }
+                            },
+                            size: 10000
+                          ).records.to_a
+                        else
+                          PurchaseOrder.search(
+                            query: {
+                              bool: {
+                                should: [
+                                  {
+                                    multi_match: {
+                                      query: query,
+                                      fields: [ "sales_channel", "status.lower" ],
+                                      fuzziness: "AUTO",
+                                      type: "best_fields"
+                                    }
+                                  }
+                                ],
+                                minimum_should_match: 1
+                              }
+                            },
+                            size: 10000
+                          ).records.to_a
+                        end
     else
                         PurchaseOrder.all.to_a
     end
@@ -38,7 +47,8 @@ class GetAllOrSearchJob
     Sidekiq.logger.info ">>>>>> Found #{purchase_orders.size} purchase orders"
 
     cache_key = query.present? ? "purchase_orders_results_#{query}" : "purchase_orders_results_all"
-    Rails.cache.write(cache_key, purchase_orders.as_json, expires_in: 2.hours)
+    Rails.cache.write(cache_key, purchase_orders.as_json(only: [ :id, :status, :sales_channel ]), expires_in: 2.hours)
+
     Sidekiq.logger.info ">>>>>> Cached #{purchase_orders.size} orders successfully"
   end
 end
--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -3,16 +3,35 @@ class PurchaseOrder < ApplicationRecord
   include Elasticsearch::Model::Callbacks
 
   # Define the Elasticsearch index structure
-  settings index: { number_of_shards: 1, number_of_replicas: 1 } do
+  # settings index: { number_of_shards: 1, number_of_replicas: 1 } do
+  #   mappings dynamic: "false" do
+  #     indexes :id, type: "integer"
+  #     indexes :order_num, type: "keyword"
+  #     indexes :purchase_date, type: "date"
+  #     indexes :status, type: "keyword"
+  #     indexes :order_total, type: "float"
+  #     indexes :ship_to_name, type: "text"
+  #     indexes :created_at, type: "date"
+  #     indexes :updated_at, type: "date"
+  #   end
+  # end
+
+  settings index: { number_of_shards: 1, number_of_replicas: 0 } do
+    settings analysis: {
+      normalizer: {
+        lowercase_normalizer: {
+          type: "custom",
+          filter: [ "lowercase" ]
+        }
+      }
+    }
+
     mappings dynamic: "false" do
-      indexes :id, type: "integer"
-      indexes :order_num, type: "keyword"
-      indexes :purchase_date, type: "date"
-      indexes :status, type: "keyword"
-      indexes :order_total, type: "float"
-      indexes :ship_to_name, type: "text"
-      indexes :created_at, type: "date"
-      indexes :updated_at, type: "date"
+      indexes :status, type: "keyword", copy_to: "all_fields", fields: {
+        lower: { type: "keyword", normalizer: "lowercase_normalizer" }
+      }
+      indexes :sales_channel, type: "keyword", copy_to: "all_fields"
+      indexes :search_all, type: "text", analyzer: "standard"
     end
   end
 end
